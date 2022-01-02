6779fae13fab741521b9a9fbc0a939dd63dfb5e8
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2022-01-02 19:10:36 +0800

    Dryfactory stagging

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString().trim() || '01234567890123456789';
+const projectId = "3f208fc43a0a432cb1d20e605269cf55";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
